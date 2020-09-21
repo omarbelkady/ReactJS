a1bb15f0cad4a638ecd69b997e500286fa2e0f01
Omar <omar.belkady@g.austincc.edu>
2020-09-21 02:46:34 +0100

    Properties

--- Create_React_App/my-app/src/App.js
+++ Create_React_App/my-app/src/App.js
@@ -1,15 +1,14 @@
-import React,{Component} from 'react';
+import React from 'react';
 import Clover from './Clover';
 
 /*This file App.js is called my root component*/
 function App() {
-	return (
-        <div className="App">
-  		<h1>My First React App</h1> 
-			<p>Learn to love 746867 like 63526</p>
-         		<Clover />
-	</div>
-	);
+  return (
+         <div className="App">
+            <h1>My First React App</h1>
+            <p>Learn to love 746867 like 63526</p>
+         </div>
+  );
 }
 
 export default App;
