81a58548e5b901d76c8dbbfb1f95b616fecc7e82
Omar <omar.belkady@g.austincc.edu>
2020-09-24 16:36:07 +0100

    How To Use Link and NavLink Tags in React JSX

---
+++ Links_Navlinks/clover/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link, NavLink} from 'react-router-dom'
+
+const Navbar = () =>{
+	return (
+		<nav className="nav-wrapper red darken-3">
+			<div className="container">
+				<a className="brand-logo">Clover</a>
+				<ul className="right">
+					<li><Link to="/">Home</Link></li>
+					<li><NavLink to="/about">About</NavLink></li>
+					<li><NavLink to="/contact">Contact</NavLink></li>
+				</ul>
+			</div>
+		</nav>
+	)
+}
+
+export default Navbar;
