a64537bc52b73856f388bcccb35cb161a799503f
Omar Belkady <31806568+omarbelkady@users.noreply.github.com>
2021-03-01 20:35:29 +0000

    How To Use Accordion, with React Hooks

---
+++ Projects/accordionproj/src/components/Accordion.jsx
@@ -0,0 +1,12 @@
+import "./Accordion.css";
+import React from "react";
+
+function template() {
+  return (
+    <div className="accordion">
+      <h1>Accordion</h1>
+    </div>
+  );
+};
+
+export default template;
